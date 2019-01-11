8e502e75c73308168f3edf6e493a491279f78af3
Jeiner Noriega <NoriegaJ@jgb.int>
2019-01-11 10:32:46 -0500

    Cleaned up main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-let toggleButton = document.querySelector('.toggle-button');
-let mobileNav = document.querySelector('.mobile-nav');
+let toggleButton = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector(".mobile-nav");
 
+let makeVisible = () => {
+  mobileNav.classList.toggle("show-mobile-nav");
+  toggleButton.classList.toggle("toggle-button__clicked");
+};
 
-let makeVisible = () =>{
-
-  mobileNav.classList.toggle('show-mobile-nav');
-  toggleButton.classList.toggle('toggle-button__clicked');
-  
-}
-
-
-toggleButton.addEventListener('click', makeVisible);
+toggleButton.addEventListener("click", makeVisible);
